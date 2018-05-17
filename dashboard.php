a745410f0cd456496f915f39983cf2bfb947b9f0
nikola1753 <tomovskinikola@gmail.com>
2018-05-17 18:27:22 +0200

    cleanup

--- dashboard.php
+++ dashboard.php
@@ -6,6 +6,20 @@
 
 ForceLogin();
 
+  $user_id = $_SESSION['user_id'];
+  $getUserInfo = $con->prepare("SELECT email, reg_time FROM users WHERE user_id = :user_id LIMIT 1");
+  $getUserInfo->bindParam(':user_id', $user_id, PDO::PARAM_INT);
+  $getUserInfo->execute();
+
+  if($getUserInfo->rowCount() == 1) {
+    // user found
+    $user = $getUserInfo->fetch(PDO::FETCH_ASSOC);
+  } else {
+    // user isn't signed in
+    header("Location: /nt/lrp/logout.php");
+    exit;
+  }
+
   //echo $_SESSION['user_id'] . " is your user id";
   //exit;
 
@@ -31,7 +45,8 @@ ForceLogin();
   <body>
   
     <div class="uk-section uk-container">
-      You're signed in as user <?php echo $_SESSION['user_id']; ?>
+      <h3>Dashboard</h3>
+      <p>Hello <?php echo $user['email']; ?>, you registered at <?php echo $user['reg_time']; ?>.</p>
     </div>
   
   <?php require_once "inc/footer.php"; ?>
