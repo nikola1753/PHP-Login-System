d6fe272f329ed600a5c5b7e551375636529f089c
nikola1753 <tomovskinikola@gmail.com>
2018-05-17 14:10:38 +0200

    added login js

--- assets/js/main.js
+++ assets/js/main.js
@@ -52,3 +52,59 @@ $(document)
   
   return false;
 })
+
+// login
+
+.on("submit", "form.js-login", function(event) {
+  event.preventDefault();
+  
+  var $form = $(this);
+  var $error = $(".js-error", $form);
+  var data = {
+    email: $("input[type='email']", $form).val(),
+    password: $("input[type='password']", $form).val()
+  }
+  
+  if (data.email.length < 6) {
+    $error.text("Please enter a valid e-mail address")
+    .show();
+    return false;
+  } else if (data.password.length < 12) {
+    $error.text("Please enter a passphrase that's at least 12 characters long")
+    .show();
+    return false;
+  }
+  
+  $error.hide();
+  
+  $.ajax({
+    type: 'POST',
+    url: '/nt/lrp/ajax/login.php',
+    data: data,
+    dataType: 'json',
+    async: true,
+  })
+  .done(function ajaxDone(data) {
+    console.log(data);
+    console.log('ajax done');
+    
+    if (data.redirect !== undefined) {
+      window.location = data.redirect;
+    } else if (data.error !== undefined) {
+      $error
+        .text(data.error)
+        .show();
+    }
+  })
+  .fail(function ajaxFailed(e) {
+    console.log(e);
+    console.log('ajax failed');
+  })
+  .always(function ajaxAlwaysDoThis(data) {
+    console.log('always');
+  })
+  
+  // alert("form was submitted");
+  
+  return false;
+})
